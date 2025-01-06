244da5d6f14cb96fb7ef87cd006df251e876f239
Eric <a0978008840@gmail.com>
2025-01-06 17:56:31 +0800

    .......

--- Untitled.sql
+++
@@ -1,7 +0,0 @@
-create table `ROLE` (
-ID int not null auto_increment comment '編號',
-`NAME` varchar(100) not null comment '角色名稱',
-primary key (ID)
-) comment = '角色資料表';
-insert into ROLE(ID, NAME)
-values (1,'ADMIN'), (2,'USER');
